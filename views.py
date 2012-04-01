990f0738faf13e607803bb58b42ca417a7dfd822
James Kingsbery <06jameskingsbery@gmail.com>
2012-04-01 17:19:16 -0400

    Got Django working on my laptop (probably a version issue)

--- views.py
+++ views.py
@@ -4,18 +4,21 @@ from django.template import Context
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
 
-from AlgoTools.objects.PSql import PSql
 from django import forms
 
 from random import random as rnd
 
 def index(request):
     things=["The Cloud","Agile","Social","Hyper-local","Scrum","Marketing Technology","Big Data","UX","SOA","SaaS"]
-    old_things=["EJB","CORBA"]
+    go_able=["Agile","Viral","Social","Public"]
+    old_things=["EJB","CORBA","68000","The CD","COBOL","The Dodo"]
+    crash_things=["Squirrel","Eclipse","Windows","Firefox","Excel","Mac","Database"]
+    crash_explitive=["Not Again!","Thanks!","Just lost all my data!"]
     x=int(rnd()*len(things))
     thing1=things[x]
     del things[x]
     thing2=things[int(rnd()*len(things))]
     tweet="When will {0} and {1} intersect?".format(thing1,thing2)
     other_tweet="When Will {0} go the way of {1}?".format(thing1,old_things[int(rnd()*len(old_things))])
-    return HttpResponse(tweet + "<br/>" + other_tweet)
+    crash_tweet="{0} crashed! {1}".format(crash_things[int(rnd()*len(crash_things))],crash_explitive[int(rnd()*len(crash_explitive))])
+    return HttpResponse(tweet + "<br/>" + other_tweet + "<br/>" + crash_tweet)
